f3f60e2fecb2a8ebad56da0d753a01a97ea7d25a
Kire <atanaskovkire@gmail.com>
2024-03-10 17:40:02 +0100

    Initial commit

---
+++ restaurant/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+
+admin.site.register(Restaurant)
+admin.site.register(Dish)
+admin.site.register(DishRestaurant)
+admin.site.register(Employee)
+admin.site.register(BusinessHours)
